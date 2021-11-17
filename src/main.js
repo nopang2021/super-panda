8d76083ebbd7ae1c9cd037d28965ed6e2c816226
Pang <nopang2021@gmail.com>
2021-11-17 10:59:23 +0400

    move router.js store.js to /plugins/

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
-import router from './router'
-import store from './store'
+import router from './plugins/router'
+import store from './plugins/store'
 
 const app = createApp(App).use(store).use(router)
 installElementPlus(app)
