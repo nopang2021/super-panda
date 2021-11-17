60926e601901aeb04f0b6bbfb6f93689f489657b
Pang <nopang2021@gmail.com>
2021-11-17 23:21:25 +0400

    install ethers

--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import installElementPlus from './plugins/element'
 import router from './plugins/router'
-import store from './plugins/store'
+import store from './store'
+
 
 const app = createApp(App).use(store).use(router)
 installElementPlus(app)
 app.mount('#app')
+
+// Initialize ethers store
+store.dispatch('ethers/init')
+
+
