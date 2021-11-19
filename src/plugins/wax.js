f75d093f05226ec99552ec6365d44b52086825ee
Pang <nopang2021@gmail.com>
2021-11-20 03:36:16 +0400

    install assetjs

--- src/plugins/wax.js
+++ src/plugins/wax.js
@@ -1,14 +1,20 @@
 import * as waxjs from "@waxio/waxjs/dist";
+import * as assetjs from "atomicassets"
 
 export default {
   install:  (app) => {
     const wax = new waxjs.WaxJS({rpcEndpoint: 'https://wax.greymass.com', tryAutoLogin: false});
+    const assetsApi = new assetjs.ExplorerApi("https://wax.api.atomicassets.io", "atomicassets", {fetch});
     app.provide('waxjs', waxjs);
+    app.provide('assetjs', assetjs);
     app.config.globalProperties.$wax = wax;
+    app.config.globalProperties.$assetsApi = assetsApi;
     // Plugin code goes here
     // console.log('app', app);
     // console.log('wax', wax);
     // console.log('waxjs', waxjs);
+    console.log(assetApi);
+    console.log(assetjs);
   }
 }
 
