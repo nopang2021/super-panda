922e3f270a85cbd2e8178fb5bdff0781a5b1604c
Pang <nopang2021@gmail.com>
2021-11-20 03:14:25 +0400

    install wax.js

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import installElementPlus from './plugins/element'
 import router from './plugins/router'
 import store from './plugins/store'
+import wax from './plugins/wax'
 
-const app = createApp(App).use(store).use(router)
+const app = createApp(App).use(store).use(router).use(wax)
 installElementPlus(app)
 app.mount('#app')
